cc03634825ab70dfcf989326409fbc737bb0273b
Ludovic Cleroux <ludydoo@gmail.com>
2022-04-10 17:53:21 +0200

    Prim's algorithm

--- pkg/prim/prim.go
+++ pkg/prim/prim.go
@@ -31,52 +31,77 @@ func PrimsAlgorithm(mat [][]int) [][]int {
 	var (
 		// number of vertices
 		numNodes int
-		// cost matrix
+
+		// store the cost of the edges
 		cost [][]int
-		// visited nodes
+
+		// store visited nodes
 		visited []bool
-		// min cost edges
-		predNode []int
+
+		// stores the edge with the lowest cost
+		// the index of the array is the index of the predecessor
+		// the value of the array is the index of the successor
+		predecessors []int
 	)
 
 	numNodes = len(mat)
 	visited = make([]bool, numNodes)
-	predNode = make([]int, numNodes)
+	predecessors = make([]int, numNodes)
 	cost = make([][]int, numNodes)
 	visited[0] = true
 
+	// init cost matrix
 	for i := 0; i < numNodes; i++ {
 		cost[i] = make([]int, numNodes)
 		for j := 0; j < numNodes; j++ {
 			cost[i][j] = mat[i][j]
+			// if i == j, set cost to infinity
 			if mat[i][j] == 0 {
 				cost[i][j] = math.MaxInt32
 			}
 		}
 	}
 
+	// loop on all nodes > 0
+	// because we start from node 0, and we want to visit only the other nodes
 	for k := 1; k < numNodes; k++ {
+
+		// x is the node with the minimum cost
+		// y is the node with the minimum cost from x
 		var x, y int
+
+		// find the minimum cost edge from any visited node to any unvisited node
+		// store the source of the edge in x and the destination in y
+
+		// loop on all nodes
 		for i := 0; i < numNodes; i++ {
 			for j := 0; j < numNodes; j++ {
+				// if the node was not visited, and the cost is lower than the current one
+				// update the current node
 				if visited[i] && !visited[j] && cost[i][j] < cost[x][y] {
+					// x is the visited node
 					x = i
+					// y is the unvisited node
 					y = j
 				}
 			}
 		}
 		println("next selected edge: ", x, ",", y, " cost: ", cost[x][y])
-		predNode[y] = x
+		// set the parent of y to x
+		predecessors[y] = x
+		// mark y as visited
 		visited[y] = true
 	}
 
+	// print the edges
 	for i := 0; i < numNodes; i++ {
-		println(predNode[i], "-->", i)
+		println(predecessors[i], "-->", i)
 	}
 
+	// build the result edges
 	var result [][]int
 	for i := 0; i < numNodes; i++ {
-		result = append(result, []int{predNode[i], i})
+		result = append(result, []int{predecessors[i], i})
 	}
 
 	return result
